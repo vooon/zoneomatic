e3c2dfeccfc09b014c38f10e8fc7b98f24e72c19
Vladimir Ermakov <vooon341@gmail.com>
2025-11-22 19:40:18 +0100

    dnsfmt: port to kong, make better cli

--- cmd/dnsfmt/main.go
+++ cmd/dnsfmt/main.go
@@ -1,35 +1,53 @@
 package main
 
 import (
-	"flag"
+	"bytes"
 	"io"
-	"log"
 	"os"
 
+	"github.com/alecthomas/kong"
 	"github.com/vooon/zoneomatic/pkg/dnsfmt"
 )
 
-var (
-	flagOrigin = flag.String("o", "", "set the origin, otherwise taken from $ORIGIN or the owner name of the SOA record.")
-	flagInc    = flag.Bool("i", true, "increase the serial")
-)
+type Cli struct {
+	Origin  string   `short:"o" name:"origin" help:"set the origin, otherwise taken from $ORIGIN or the owner name of the SOA record."`
+	Inc     bool     `short:"i" name:"inc" default:"true" negatable:"" help:"increase the serial, default: ${default}"`
+	Replace bool     `short:"r" name:"replace" help:"Replace file with formatted output"`
+	Files   []string `arg:"" optional:"" placeholder:"FILE" type:"existingfile" help:"Zone file, use stdin if it is '-' or empty"`
+}
 
 func main() {
-	flag.Parse()
-	if flag.NArg() == 0 {
+	var cli Cli
+
+	kctx := kong.Parse(&cli,
+		kong.Description("DNS Zone file formatter"),
+		kong.DefaultEnvars("DNSFMT"),
+	)
+
+	if len(cli.Files) == 0 || (len(cli.Files) == 1 && cli.Files[0] == "-") {
 		data, err := io.ReadAll(os.Stdin)
-		if err != nil {
-			log.Fatalf("dnsfmt: %s", err)
-		}
-		_ = dnsfmt.Reformat(data, []byte(*flagOrigin), os.Stdout, *flagInc)
+		kctx.FatalIfErrorf(err)
+
+		err = dnsfmt.Reformat(data, []byte(cli.Origin), os.Stdout, cli.Inc)
+		kctx.FatalIfErrorf(err)
 		return
 	}
 
-	for _, a := range flag.Args() {
+	for _, a := range cli.Files {
 		data, err := os.ReadFile(a)
-		if err != nil {
-			log.Fatalf("dnsfmt: %s", err)
+		kctx.FatalIfErrorf(err)
+
+		var buf = bytes.NewBuffer(nil)
+
+		err = dnsfmt.Reformat(data, []byte(cli.Origin), buf, cli.Inc)
+		kctx.FatalIfErrorf(err)
+
+		if cli.Replace {
+			err = os.WriteFile(a, buf.Bytes(), 0644)
+			kctx.FatalIfErrorf(err)
+		} else {
+			_, err = io.Copy(os.Stdout, buf)
+			kctx.FatalIfErrorf(err)
 		}
-		_ = dnsfmt.Reformat(data, []byte(*flagOrigin), os.Stdout, *flagInc)
 	}
 }
